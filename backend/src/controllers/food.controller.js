e3adbc1015ad0d18ab6967e1582aa6714046a8ce
Amarendra-das <b122013@iiit-bh.ac.in>
2025-09-09 01:23:04 +0530

    add foodItem

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -9,9 +9,17 @@ async function createFood(req,res){
 
     const fileUploadResult = await storageService.uploadFile(req.file.buffer,uuid());
 
-    console.log(fileUploadResult);
+    const foodItem = await  foodModel.create({
+        name:req.body.name,
+        description:req.body.description,
+        video: fileUploadResult.url,
+        foodPartner:req.foodPartner._id
+    })
 
-     res.send("food item created")
+res.status(201).json({
+    message:"food created Successfully",
+    food: foodItem,
+})
 
 }
 
